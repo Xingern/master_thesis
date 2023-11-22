14d2270232faaa6c79b8ef2aab23c4620d02f979
xingern <xingdotmail@gmail.com>
2023-11-22 12:18:36 +0100

    Added some function to automatically drop the bad columns

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd
 
+# These columns are badly formatted and will be dropped by default
+DROP_COLS = ["Desorber-Packing-TT1030A", 
+             "Downstream-Lean-PT1010", 
+             "Desorber-Packing-TT1030C"]
+
 def convert_to_float_or_nan(value):
     """
     Function to convert a single value to float64, or NaN if it can't be 
@@ -11,18 +16,23 @@ def convert_to_float_or_nan(value):
     except (ValueError, TypeError):
         return np.nan
 
-def initial_clean(raw_df):
+def initial_clean(raw_df, drop_cols=DROP_COLS):
     """
     The functions does some basic pre-processing of the imported data. 
     It applies the following methods:
         1. Formats the time column to DateTime.
         2. Replaces all "Bad input" or similar values with NaN
         3. Downcasts all data to float32 for memory optimization
+        4. Drops selected columns
+        5. Removes rows with NaN or negative values
     """
+    
     raw_df["Time"] = pd.to_datetime(raw_df["Time"]).dt.strftime('%Y-%m-%d %H:%M')
     raw_df.iloc[:, 1:] = raw_df.iloc[:, 1:].applymap(convert_to_float_or_nan)
     raw_df.iloc[:, 1:] = raw_df.iloc[:, 1:].astype('float32')
-    return raw_df.copy()
+    drop_df = raw_df.drop(columns=drop_cols)
+    df = drop_nan_neg(drop_df)
+    return df.copy()
 
 def drop_nan_neg(df):
     """
@@ -31,4 +41,3 @@ def drop_nan_neg(df):
     drop_nan_df = df.dropna()
     clean_df = drop_nan_df[(drop_nan_df.iloc[:, 1:] > 0).all(1)]
     return clean_df
-
